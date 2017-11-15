00733e72d1d64f4bdadad5aeb6a21a344e058271
Christian Mihigo <mihigo@u.northwestern.edu>
2017-11-15 17:33:29 -0600

    loginANDnavbar

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,30 +2,54 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
-
+import { Button, Form, Grid, Header, Segment, Icon } from 'semantic-ui-react'
 /**
  * COMPONENT
  */
 const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+  const {name, displayName, handleSubmit} = props
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email"><small>Email</small></label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password"><small>Password</small></label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
+    <div className="login-form">
+    <Grid
+      textAlign="center"
+      style={{ height: '650px', width: '1050px'}}
+      verticalAlign="middle"
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="green" textAlign="center">
+          {displayName}
+        </Header>
+        <Form onSubmit={handleSubmit} name={name} size="large">
+          <Segment stacked style={{ height: '175px' }}>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+              type="text"
+              name="email"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              name="pass"
+            />
+
+            <Button href="/auth/spotify" color="green" fluid size="large">
+            <Icon name="spotify" size="large" color="white" />
+              Spotify {displayName}
+            </Button>
+
+          </Segment>
+
+        </Form>
+
+      </Grid.Column>
+    </Grid>
     </div>
   )
 }
---
+++ client/components/placeholder.jsx
@@ -0,0 +1,73 @@
+<div className="login-form">
+<Grid
+  textAlign="center"
+  style={{ height: '450px' }}
+  verticalAlign="middle"
+>
+  <Grid.Column style={{ maxWidth: 450 }}>
+    <Header as="h2" color="green" textAlign="center">
+      {displayName}
+    </Header>
+    <Form onSubmit={handleSubmit} name={name} size="large">
+      <Segment stacked style={{ height: '225px' }}>
+        <Form.Input
+          fluid
+          icon="user"
+          iconPosition="left"
+          placeholder="E-mail address"
+          type="text"
+          name="email"
+        />
+        <Form.Input
+          fluid
+          icon="lock"
+          iconPosition="left"
+          placeholder="Password"
+          type="password"
+          name="pass"
+        />
+
+        <Button href="/auth/spotify" color="green" fluid size="large">
+        <Icon name="spotify" size="large" color="green" />
+          Spotify {displayName}
+        </Button>
+
+      </Segment>
+
+    </Form>
+
+  </Grid.Column>
+</Grid>
+</div>
+
+
+
+
+
+
+<Form onSubmit={evt => handleSubmit(name, evt)} name={name} size="large">
+
+
+
+
+<div>
+<form onSubmit={handleSubmit} name={name}>
+  <div>
+    <label htmlFor="email"><small>Email</small></label>
+    <input name="email" type="text" />
+  </div>
+  <div>
+    <label htmlFor="password"><small>Password</small></label>
+    <input name="password" type="password" />
+  </div>
+  <div>
+    <button type="submit">{displayName}</button>
+  </div>
+  {error && error.response && <div> {error.response.data} </div>}
+</form>
+<a href="/auth/spotify">{displayName} with Spotify</a>
+</div>
+
+
+width={6}
+, width: '500px'
