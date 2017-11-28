cf87964c90bd76888e5cfd37c88dd4fa5b3faf97
mtamahori <mtamahori@gmaigmail.com>
2017-11-28 14:03:27 -0600

    cleanup event list, fix user home

--- client/components/eventList.js
+++ client/components/eventList.js
@@ -24,44 +24,37 @@ class EventList extends Component {
         <Segment textAlign="center" inverted style={{ marginTop: 'inherit' }}>
           <Header as="h1" icon="list ul" content="Your Events" textAlign="center" />
         </Segment>
+
         <Segment className="invited-events" inverted>
           <Segment compact={true} inverted>
             <Header as="h2" content="Invitations" color="purple" />
-
           </Segment>
           <List divided inverted>
             {events &&
               events.map((event) => (
                 (event.eventUser.isAttending === null && !event.hasEnded) &&
                 <List.Item key={event.id}>
-                  <Header as="h3" icon="sound" content={event.name} />
-                  <List.Content floated="left">
-                    <Button
-                      positive
-                      onClick={evt => {
-                      handleSubmit(evt, event.id, user.id, rsvpYes)
-                      history.push(`/events/${event.id}`)
-                    }}>Yes</Button>
-                    <Button negative onClick={evt => handleSubmit(evt, event.id, user.id, rsvpNo)}>No</Button>
-                  </List.Content>
+                  <Header as="h3" icon="sound" content={event.name} style={{marginBottom: '1em'}} />
                   <List.Content>
-                    <List.Header>
-                      {event.name}
+                    <List.Header style={{marginLeft: '3em'}}>
+                      <Button
+                        positive
+                        onClick={evt => {
+                          handleSubmit(evt, event.id, user.id, rsvpYes)
+                          history.push(`/events/${event.id}`)
+                        }}>Yes</Button>
+                      <Button negative onClick={evt => handleSubmit(evt, event.id, user.id, rsvpNo)}>No</Button>
                     </List.Header>
-                    <List.Description className="event-description">
+                    <List.Description>
                       <List.List as="ul">
-                        <div className="type-container">
-                          <List.Item as="li">Type: {event.type}</List.Item>
-                          <List.Item as="li">Genres: {
-                            event.genres.map(genre => (
-                              `${genre.toUpperCase()} `
-                            ))
-                          }
-                          </List.Item>
-                        </div>
-                        <List.Item as="li">
-                          <List.Content>Date: {event.date} </List.Content>
+                        <List.Item as="li">Description: {event.type}</List.Item>
+                        <List.Item as="li">Genres: {
+                          event.genres.map(genre => (
+                            `${genre.toUpperCase()} `
+                          ))
+                        }
                         </List.Item>
+                        <List.Item as="li">Date: {event.date}</List.Item>
                       </List.List>
                     </List.Description>
                   </List.Content>
@@ -71,7 +64,7 @@ class EventList extends Component {
           </List>
         </Segment>
 
-        <Segment className="upcoming-events" inverted>
+        <Segment className="hosted-events" inverted>
           <Segment compact={true} inverted>
             <Header as="h2" content="Hosted Events" color="purple" />
           </Segment>
@@ -80,31 +73,26 @@ class EventList extends Component {
               events.map((event) => (
                 (event.eventUser.isHost && !event.hasEnded) &&
                 <List.Item key={event.id}>
-                  <Header as="h3" icon="sound" content={event.name} />
-
+                  <Header as="h3" icon="sound" content={event.name} style={{marginBottom: '1em'}} />
                   <List.Content floated="left">
-                    <List.Header>
+                    <List.Header style={{marginLeft: '3em'}}>
                       <Button color="green" onClick={() => history.push(`/${event.id}/users/invite`)}>Invite Users</Button>
                       <Button negative onClick={() => handleCancelEvent(event.id, user.id)}>Cancel Event</Button>
                       <Button color="blue" onClick={() => history.push(`/events/${event.id}`)}>Set Music Preferences</Button>
                       <Button color="teal" onClick={() => history.push(`/events/${event.id}/partyview`)}>Party View</Button>
                     </List.Header>
                     <List.Description>
-                      <List.List as="ul">
-                        <div className="type-container">
-                          <List.Item as="li">Type: {event.type}</List.Item>
-                          <List.Item as="li">Genres: {
-                            event.genres.map(genre => (
-                              `${genre.toUpperCase()} `
-                            ))
-                          }
-                          </List.Item>
-                        </div>
-                        <List.Item as="li">
-                          <List.Content>Date {event.date}</List.Content>
-                        </List.Item>
-                      </List.List>
-                    </List.Description>
+                    <List.List as="ul">
+                      <List.Item as="li">Description: {event.type}</List.Item>
+                      <List.Item as="li">Genres: {
+                        event.genres.map(genre => (
+                          `${genre.toUpperCase()} `
+                        ))
+                      }
+                      </List.Item>
+                      <List.Item as="li">Date: {event.date}</List.Item>
+                    </List.List>
+                  </List.Description>
                   </List.Content>
                 </List.Item>
               ))
@@ -121,31 +109,24 @@ class EventList extends Component {
               events.map((event) => (
                 (event.eventUser.isAttending && !event.eventUser.isHost && !event.hasEnded) &&
                 <List.Item key={event.id}>
-                  <Header as="h3" icon="sound" content={event.name} />
-
+                  <Header as="h3" icon="sound" content={event.name} style={{marginBottom: '1em'}} />
                   <List.Content floated="left">
-
-
-                    <List.Header>
+                    <List.Header style={{marginLeft: '3em'}}>
                       <Button color="blue" onClick={() => history.push(`/events/${event.id}`)}>Set Music Preferences</Button>
                       <Button color="purple" onClick={() => history.push(`/events/${event.id}/partyview`)}>Party View</Button>
                     </List.Header>
                     <List.Description>
-                      <List.List as="ul">
-                        <div className="type-container">
-                          <List.Item as="li">Type: {event.type}</List.Item>
-                          <List.Item as="li">Genres: {
-                            event.genres.map(genre => (
-                              `${genre.toUpperCase()} `
-                            ))
-                          }
-                          </List.Item>
-                        </div>
-                        <List.Item as="li">
-                          <List.Content>Date: {event.date}</List.Content>
-                        </List.Item>
-                      </List.List>
-                    </List.Description>
+                    <List.List as="ul">
+                      <List.Item as="li">Description: {event.type}</List.Item>
+                      <List.Item as="li">Genres: {
+                        event.genres.map(genre => (
+                          `${genre.toUpperCase()} `
+                        ))
+                      }
+                      </List.Item>
+                      <List.Item as="li">Date: {event.date}</List.Item>
+                    </List.List>
+                  </List.Description>
                   </List.Content>
                 </List.Item>
               ))
@@ -162,26 +143,20 @@ class EventList extends Component {
               events.map((event) => (
                 (event.eventUser.isAttending && event.hasEnded) &&
                 <List.Item key={event.id}>
-                  <Header as="h3" icon="sound" content={event.name} />
-
+                  <Header as="h3" icon="sound" content={event.name} style={{marginBottom: '1em'}} />
                   <List.Content floated="left">
-
-                    <List.Description>
-                      <List.List as="ul">
-                        <div className="type-container">
-                          <List.Item as="li">Type: {event.type}</List.Item>
-                          <List.Item as="li">Genres: {
-                            event.genres.map(genre => (
-                              `${genre.toUpperCase()} `
-                            ))
-                          }
-                          </List.Item>
-                        </div>
-                        <List.Item as="li">
-                          <List.Content>Date: {event.date}</List.Content>
-                        </List.Item>
-                      </List.List>
-                    </List.Description>
+                  <List.Description>
+                  <List.List as="ul">
+                    <List.Item as="li">Description: {event.type}</List.Item>
+                    <List.Item as="li">Genres: {
+                      event.genres.map(genre => (
+                        `${genre.toUpperCase()} `
+                      ))
+                    }
+                    </List.Item>
+                    <List.Item as="li">Date: {event.date}</List.Item>
+                  </List.List>
+                </List.Description>
                   </List.Content>
                 </List.Item>
               ))
